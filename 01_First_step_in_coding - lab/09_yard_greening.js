00565dd3338809af9083b287dcd8ba10f8e5f812
Roburt96 <roburtsulev96@gmail.com>
2022-12-26 23:24:01 +0200

    100/100

--- 01_First_step_in_coding - lab/09_yard_greening.js
+++ 01_First_step_in_coding - lab/09_yard_greening.js
@@ -1,5 +1,9 @@
 function greening(input) {
-    let per_m = 7.61;
-    let discount = 18;
- 
-greening(["550"])
+    let totalPrice = Number(input) * 7.61;
+    let discount = totalPrice * 0.18;
+    let totalPriceDiscount = totalPrice - discount;
+
+    console.log(`The final price is: ${totalPriceDiscount} lv.`);
+    console.log(`The discount is: ${discount} lv.`);
+}
+greening(["550"]);
