c755c2a435ddd4a158501ba79531956a5984dd15
Roburt96 <roburtsulev96@gmail.com>
2023-04-29 18:49:54 +0300

    done

---
+++ 02_JS_Fundamentals/01_Basic_Syntax_Conditional_Statements_and_Loops/Basic Syntax, Conditional Statements and Loops - Lab/05_Month_Printer.js
@@ -0,0 +1,22 @@
+function month(number){
+    this.dictionary = {
+        1: 'January',
+        2: 'February',
+        3: 'March',
+        4: 'April',
+        5: 'May',
+        6: 'June',
+        7: 'July',
+        8: 'August',
+        9: 'September',
+        10: 'October',
+        11: 'November',
+        12: 'December'
+    }
+    if (number < 1 || number > 12) {
+        return 'Error!'
+    } else {
+        return this.dictionary[number]
+}
+}
+console.log(month(13));
