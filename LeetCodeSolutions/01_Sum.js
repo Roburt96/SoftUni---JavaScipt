65eb946e09e576b054b6ffc9e9bc35fd62ae924e
Roburt96 <110181057+Roburt96@users.noreply.github.com>
2023-04-30 10:04:07 +0300

    done

---
+++ LeetCodeSolutions/01_Sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (complement in map) {
+      return [map[complement], i];
+    }
+    map[nums[i]] = i;
+  }
+  return null;
+};
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+console.log(twoSum(nums, target));
